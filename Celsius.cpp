f55b51f074eb78a2ebfb6367a353f0f04a431aba
Meenyodsathon <54592907+Meenyodsathon@users.noreply.github.com>
2019-08-27 23:20:17 +0700

    Add files via upload

---
+++ Celsius.cpp
@@ -0,0 +1,16 @@
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	float C;
+	cout << "Enter Celsius temperature : ";
+	cin >> C;
+	double Result (0);
+	Result = (1.8*C)+32 ;
+	cout << "Fahrenheit =" << Result << endl;
+	cout << "Now weather in thailand is " << ((Result>70)? "HOT" : "COOL")<<endl;
+	return(0);
+}
+
